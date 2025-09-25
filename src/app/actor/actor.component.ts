c90adb973590b80bd541819e4ddb05f13a4588f6
Vámosi László Ádám <vamosilaszloadam@outlook.com>
2025-09-25 10:56:08 +0200

    Kész

--- src/app/actor/actor.component.ts
+++ src/app/actor/actor.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../shared/api.service';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-actor',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './actor.component.html',
   styleUrl: './actor.component.css'
 })
 export class ActorComponent {
 
   actors: any;
-  constructor(private api: ApiService){}
+  actorForm: any;
+  constructor(
+    private api: ApiService,
+    private builder: FormBuilder
+  ){}
 
   ngOnInit(){
     this.getActors()
+    this.actorForm = this.builder.group({
+      name: '',
+      city: '',
+      birthDate: ''
+    })
   }
   
   getActors() {
@@ -24,6 +34,24 @@ export class ActorComponent {
         this.actors = result.data
       },
       // error: () => {}
+    })
+  }
+
+  addActor() {
+    // console.log('műkszik')
+    console.log(this.actorForm.value)
+    const newActor = {
+      name: this.actorForm.value.name,
+      city: this.actorForm.value.city,
+      birth: this.actorForm.value.birthDate
+    }
+    this.api.addActor(newActor)
+    this.api.addActor(this.actorForm.value).subscribe({
+      next: (result) => {
+        console.log(result)
+        this.getActors()
+        this.actorForm.reset()
+      }
     })
   }
 }
